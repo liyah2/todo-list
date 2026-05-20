8bdaad6335cf2b650495eacbbfaaa8285ac09941
liyah2 <aaliyahcloss@gmail.com>
2026-05-19 20:09:05 -0400

    App.jsx edited, header + todospage imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,12 @@
-import { useState } from "react";
 import "./App.css";
-import TodoForm from "./features/Todos/TodoForm";
-import TodoList from "./features/Todos/TodoList/TodoList";
+import Header from "./shared/Header";
+import TodosPage from "./features/Todos/TodosPage";
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
-
-  function addTodo(todoTitle) {
-    const newTodo = {
-      id: Date.now(),
-      title: todoTitle,
-      isCompleted: false,
-    };
-
-    setTodoList((previous) => [newTodo, ...previous]);
-  }
-
-  function completeTodo(id) {
-    const updatedTodoList = todoList.map((todo) => {
-      if (todo.id === id) {
-        return { ...todo, isCompleted: true };
-      }
-      return todo;
-    });
-    setTodoList(updatedTodoList);
-  }
-
-  const updateTodo = (editedTodo) => {
-    const updatedTodos = todoList.map((todo) => {
-      if (todo.id === editedTodo.id) {
-        return { ...editedTodo };
-      }
-
-      return todo;
-    });
-
-    setTodoList(updatedTodos);
-  };
-
   return (
     <div>
-      <h1>Todo List</h1>
-      <TodoForm onAddTodo={addTodo} />
-
-      <TodoList
-        todoList={todoList}
-        onCompleteTodo={completeTodo}
-        onUpdateTodo={updateTodo}
-      />
+      <Header />
+      <TodosPage />
     </div>
   );
 }

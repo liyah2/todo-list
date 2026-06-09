86902aec73179e49542b551e8f052c3d702fae57
liyah2 <aaliyahcloss@gmail.com>
2026-06-09 18:54:08 -0400

    AboutPage info updated

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,25 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h2>About This App</h2>
+      <p>
+        This todo app helps users manage tasks with authentication, filtering,
+        sorting, and routing.
+      </p>
+
+      <h3>Features</h3>
+      <ul>
+        <li>Add,edit, and complete todos</li>
+        <li>Search and sort todos</li>
+        <li>Protected routes</li>
+      </ul>
+
+      <h3>Technologies</h3>
+      <ul>
+        <li>React</li>
+        <li>React Router</li>
+        <li>Vite</li>
+      </ul>
+    </div>
+  );
+}

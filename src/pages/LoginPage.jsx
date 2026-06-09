bfa0423e880cf348aa3ac48b67bbce04bfd96005
liyah2 <aaliyahcloss@gmail.com>
2026-06-09 19:46:08 -0400

    Login updated, requires login, routes back

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,19 @@
+import { useEffect } from "react";
+import { useNavigate, useLocation } from "react-router";
+import { useAuth } from "../reducers/contexts/AuthContext";
 import Logon from "../features/Logon";
 
 export default function LoginPage() {
+  const { iseAuthenticated } = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const from = location.state?.from?.pathname || "/todos";
+
+  useEffect(() => {
+    if (iseAuthenticated) {
+      navigate(from, { replace: true });
+    }
+  }, [iseAuthenticated, navigate, from]);
   return <Logon />;
 }

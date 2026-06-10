117d5a5b33d65914d18d79f38c8e4920a532395f
liyah2 <aaliyahcloss@gmail.com>
2026-06-09 20:42:57 -0400

    fixed navigation

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -8,6 +8,8 @@ export default function Header() {
     <header>
       <h1>Todo List</h1>
 
+      <Navigation />
+
       {isAuthenticated && (
         <button type="button" onClick={logout}>
           Log Out
